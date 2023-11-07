4b22812763e9f26f24c653a84614ffc108f73044
AndyPecotche <pecotcheandres@outlook.com>
2023-11-07 20:23:23 -0300

    .

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,4 +1,8 @@
 class DogsController < ApplicationController
+  
+  before_action :authenticate_user!
+  before_action :admin_user, only: [:new, :create]
+
     def new
         #@user = User.find(params[:user_id]) # Obtén el usuario a partir del ID pasado como parámetro
         @dog = Dog.new # Crea un nuevo perro
@@ -23,8 +27,13 @@ class DogsController < ApplicationController
     #@dogs = Dog.where(user_id: user_id)
     #se usa la siguiente opcion ya que se utiliza el @user en la vista para listar el nombre del cliente, sino la de arriba funciona tambien
     @user = User.find(params[:user_id]) # Obtén el usuario a partir del ID pasado como parámetro
-    @dogs = @user.dogs
-    
+    if (current_user.es_admin? || current_user.id == @user.id)
+      @user = User.find(params[:user_id]) # Obtén el usuario a partir del ID pasado como parámetro
+      @dogs = @user.dogs
+    else
+      flash[:alert] = 'Acceso no autorizado para ver esta lista de perros'
+      redirect_to root_path
+    end
   end
   
   private
@@ -32,4 +41,12 @@ class DogsController < ApplicationController
   def dog_params
     params.require(:dog).permit(:name, :breed, :birthdate, :image, :user_id)
   end
+
+  def admin_user
+    unless current_user && current_user.es_admin?
+      flash[:alert] = 'Acceso no autorizado'
+      redirect_to root_path
+    end
+  end
+
 end
