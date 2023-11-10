4b502eaa72256b54675ff7ef34649dad29335429
AndyPecotche <pecotcheandres@outlook.com>
2023-11-09 21:28:32 -0300

    .

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -8,30 +8,45 @@ class AppointmentsController < ApplicationController
     Rails.logger.debug("entro------a new-------------------------------------------------------------------------------")
     @appointment = Appointment.new
     @user_dogs = current_user.dogs
-    if Appointment.where(user_id: current_user.id, state: 'en_espera')
-      Rails.logger.debug("entro------a if--ya tineeeeee-----------------------------------------------------------------------------")
-      @ya_tiene_turno = true
-    end
   end
 
   def create
     Rails.logger.debug("entro------a create-------------------------------------------------------------------------------")
     @appointment = Appointment.new(appointment_params)
     @appointment.user_id = current_user.id
-    if Appointment.where(user_id: current_user.id, state: 'en_espera')
-      Rails.logger.debug("entro------a if--ya tineeeeee-----------------------------------------------------------------------------")
-      @ya_tiene_turno = true
-    end
-    if @appointment.save
-      Rails.logger.debug("entro------a save--create-----------------------------------------------------------------------------")
-      redirect_to root_path
-      flash[:notice] = "Se ha creado el turno"
+    if !@appointment.dog_ids.empty?
+      @perro = []
+      @appointment.dog_ids.each do |dog_id|
+        if @ultimo_turno_del_perro_en_join = AppointmentsDog.where(dog_id: dog_id).order(created_at: :desc).first
+          @id_del_ultimo_turno_del_perro = @ultimo_turno_del_perro_en_join.appointment_id
+          if Appointment.find(@id_del_ultimo_turno_del_perro).state == 'en_espera'
+            @perro << Dog.find(dog_id).name
+          end
+        end
+      end
+      if !@perro.any?
+        if @appointment.save
+          Rails.logger.debug("entro------a save--create-----------------------------------------------------------------------------")
+          redirect_to new_appointment_path
+          flash[:notice] = "Se ha creado el turno"
+        else
+          #render 'new'
+          redirect_to root_path
+          error = @appointment.errors.full_messages
+          flash[:notice] = error
+          Rails.logger.debug("entro------a error--create-----------------------------------------------------------------------------")
+        end
+      else
+        redirect_to new_appointment_path
+        @perro.each do |perro|
+          flash[:notice] = "El perro #{@perro} ya tiene un turno en espera"
+        end
+      end
     else
-      #render 'new'
-      redirect_to root_path
-      error = @appointment.errors.full_messages
-      flash[:notice] = error
-      Rails.logger.debug("entro------a error--create-----------------------------------------------------------------------------")
+      Rails.logger.debug("entro------a else--es de perros empty-----------------------------------------------------------------------------")
+      #redirect_to new_appointment_path
+      redirect_to new_appointment_path
+      flash[:notice] = "Debe seleccionar al menos un perro"
     end
   end
 
