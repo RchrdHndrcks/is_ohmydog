1cbd15020f3faacf57a579087500c3b7d0f15906
AndyPecotche <pecotcheandres@outlook.com>
2023-11-18 23:52:44 -0300

    se agrega funcion cancelar turnos

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,6 +1,6 @@
 # app/models/appointment.rb
 class Appointment < ApplicationRecord
-    enum state: { en_espera: 0, aceptado: 1, rechazado: 2 }
+    enum state: { en_espera: 0, aceptado: 1, rechazado: 2, cancelado: 3 }
     enum timeSlot: { mañana: 0, tarde: 1 }
     attribute :appointment_date, :datetime
     has_and_belongs_to_many :dogs
