b3beb1f2c60247f6f32c468638ad0db39fcde46c
RchrdHndrcks <115677243+RchrdHndrcks@users.noreply.github.com>
2023-11-23 20:43:36 -0300

    Feature/add edit password (#37)
    * add password modification
    
    * modify view to show password fields
    
    * add validation for change password or not
    
    * a
    
    * add view
    
    ---------
    
    Co-authored-by: RchrdHndrcks <francodamianniz@alu.ing.unlp.edu.ar>

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -26,27 +26,35 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def edit
+    @page_title = "Editar usuario"
     @user = User.find(params[:id])
   end
 
   def update
     @user = User.find(params[:user][:id])
     
-    # Si el checkbox no está marcado, elimina los campos relacionados con la contraseña
     if params[:user][:change_password_checkbox] == "1"
+      if params[:user][:current_password].blank?
+        flash[:alert] = "Debes proporcionar la contraseña actual para cambiar la contraseña."
+        render :edit and return
+      end
       update_password
     else
       params[:user].delete(:current_password)
       params[:user].delete(:password)
       params[:user].delete(:password_confirmation)
     end
-
+  
     if @user.update(user_params)
-      redirect_to root_path
+      redirect_to users_show_path(user_id: @user.id)
       set_flash_message! :notice, :updated
     else
       render :edit
     end
+  end
+
+  def after_sign_up_path_for(resource)
+    user_show_path(user_id: resource.id)
   end
   
   private
@@ -59,9 +67,15 @@ class RegistrationsController < Devise::RegistrationsController
     end
   end
 
+  def password_params
+    params.require(:user).permit(:current_password, :password, :password_confirmation)
+  end
+
   def user_params
     params.require(:user).permit(:id, :name, :last_name, :identifier_number, :address, :phone_number, :email, :password, :password_confirmation, :es_admin)
+    params.require(:user).permit(:id, :name, :last_name, :identifier_number, :address, :phone_number, :email, :password, :password_confirmation, :es_admin)
   end
+  
   
   def check_admin
     Rails.logger.debug("entro----check-admin--------------------------------------------------------------------------------")
