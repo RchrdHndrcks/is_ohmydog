d6d84a730be309194210172645fd9dbfcc65aec3
Andy <108961433+AndyPecotche@users.noreply.github.com>
2023-12-03 20:20:40 -0300

    Feature/motivo turno (#46)
    * se agrega razon a los turnos
    
    * faltaba schema

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,5 +3,6 @@ class Appointment < ApplicationRecord
     enum state: { en_espera: 0, aceptado: 1, rechazado: 2, cancelado: 3 }
     enum timeSlot: { mañana: 0, tarde: 1 }
     attribute :appointment_date, :datetime
+    enum reason: { urgencia: 0, consulta_general: 1, castración: 2, vacunación: 3 }
     has_and_belongs_to_many :dogs
 end
--- db/schema.rb
+++ db/schema.rb
@@ -63,6 +63,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_25_024346) do
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
     t.integer "user_id"
+    t.integer "reason"
     t.index ["user_id"], name: "index_appointments_on_user_id"
   end
 
@@ -127,9 +128,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_25_024346) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-# Could not dump table "users" because of following StandardError
-#   Unknown type 'news' for column 'news'
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
